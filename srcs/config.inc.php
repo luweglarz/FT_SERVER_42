16651f9ac223a20ed735a10255a0e2b56540ef3a
RolandGamos <lucasweglarz9@gmail.com>
2020-09-29 14:56:08 +0200

    blowfish_secret generated

--- srcs/config.inc.php
+++ srcs/config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'h-D]J-0YY7VCdU0E7Sw-MctGFg1okT{A'; 
 
 /**
  * Servers configuration
