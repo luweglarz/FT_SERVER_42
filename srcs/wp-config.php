5ac10fc7aefea7e100c37399939f1d8607e416ff
RolandGamos <lucasweglarz9@gmail.com>
2020-10-01 12:31:31 +0200

    Wordpress works

--- srcs/wp-config.php
+++ srcs/wp-config.php
@@ -37,8 +37,6 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-define('WP_ALLOW_REPAIR', true);
-
 /**#@+
  * Authentication Unique Keys and Salts.
  *
